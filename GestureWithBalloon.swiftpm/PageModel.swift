79cc533c134bf2073cf512c09bc748e779fb4586
Rookie <jisu7408@gmail.com>
2022-05-09 21:41:17 +0900

    5.09 WWDC Refactoring
    Revision
    
    1. PageModel 수정
    - StoryPage 구조체의 text 프로퍼티를 삭제

--- GestureWithBalloon.swiftpm/PageModel.swift
+++ GestureWithBalloon.swiftpm/PageModel.swift
@@ -12,23 +12,22 @@ struct Story {
     
     let pages: [StoryPage]
     
-    subscript(_ pageIndex: Int) -> StoryPage {
+    subscript(pageIndex: Int) -> StoryPage {
         return pages[pageIndex]
     }
 }
 
 struct StoryPage {
-    let text: String
     
     let choices: [Choice]
     
-    init(_ text: String, choices: [Choice]) {
-        self.text = text
+    init(_ choices: [Choice]) {
         self.choices = choices
     }
 }
 
-struct Choice {
+struct Choice : Hashable {
+    
     let text: String
     let destination: Int
 }
